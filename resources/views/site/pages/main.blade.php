7901b73160ac502f8af45bc2cc883eed7d70ee74
Erfan Ebrahimi <persionhost@gmail.com>
2023-11-08 01:40:37 +0330

    fix search

--- resources/views/site/pages/main.blade.php
+++ resources/views/site/pages/main.blade.php
@@ -1,57 +1,86 @@
 @extends('site.layout.master')
 
 @isset($company)
-@section('title', $company->company_name . ' | ' . $company->company_phone)
+    @section('title', $company->company_name . ' | ' . $company->company_phone)
 @endisset
 @isset($required_for_rent)
-@section('title', __('required_for_rent_page_title'))
+    @section('title', __('required_for_rent_page_title'))
 @endisset
 
 @section('content')
 
-<div  x-data="{
-      areas: [],
-      async fetchAreas(){
-        fetch('{{ asset('/api/v1/cities') }}')
-        .then( response => response.json() )
-        .then( data => this.areas = data.data );
-      },
-      types: [],
-      async fetchTypes(){
-        fetch('{{ asset('/api/v1/get-search-property') }}')
-        .then( response => response.json() )
-        .then( data => this.types = data.data.type );
-      },
-    }" x-init="fetchAreas();fetchTypes();">
-    @isset($company)
-{{--    @include('site.sections.company-info')--}}
-{{--        <h2>{{__('company_ads')}}</h2>--}}
-    @endisset
+    <div x-data="{
+            selectedCity : null,
+            selectedCityObject : null,
+            selectedType : null,
+            selectedTypeObject : null,
+            isRequiredPage : false,
+            selectedPurpose : '{{ request()->get('type') }}',
+            advertise: {},
+            areas: [],
+            async fetchAreas(){
+                fetch('{{ asset('/api/v1/cities') }}')
+                .then( response => response.json() )
+                .then( data => this.areas = data.data );
+                },
+            types: [],
+            async fetchTypes(){
+                fetch('{{ asset('/api/v1/get-search-property') }}')
+                .then( response => response.json() )
+                .then( data => this.types = data.data.type );
+            },
+            async search(){
+                fetch('{{ asset('/api/v1/search-advertising') }}', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        area_id: this.selectedCity > 0 ? [this.selectedCity] : null,
+                        service_category_id:this.selectedType,
+                        isRequiredPage: this.isRequiredPage,
+                        purpose: this.selectedPurpose
+                    })
+                })
+                .then( response => response.json() )
+                .then( data => this.advertise = data.data );
+            }
+        }" x-init="fetchAreas();fetchTypes();search();">
+        @isset($company)
+            {{--    @include('site.sections.company-info')--}}
+            {{--        <h2>{{__('company_ads')}}</h2>--}}
+        @endisset
 
-    @if(! isset($company))
-    @include('site.sections.search')
-    @endif
+        @if(! isset($company))
+            @include('site.sections.search')
+        @endif
 
-    <section class="mt-10">
-        <div class="container">
-            <div class="row">
-                <div class="col-6">
-                    <h3>أحدت الإعلانات</h3>
-                </div>
-                <div class="col-6 text-left">
-                    <small> عقارات للايجارفي اكويت (2121 إعلان)</small>
+        <section class="mt-10">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <h3>أحدت الإعلانات</h3>
+                    </div>
+                    <div class="col-6 text-left">
+                        <small>
+                            <span x-text="selectedTypeObject ? selectedTypeObject.title_{{ app()->getLocale() }} : '' "></span>
+                            <span x-text="selectedPurpose == 'rent' ? '{{ __('rent') }}' : ( selectedPurpose == 'sell' ? '{{ __('sell') }}' : ( selectedPurpose == 'exchange' ? '{{ __('exchange') }}' : '' ) )" ></span>
+                            <span>{{ __('search_in') }}</span>
+                            <span x-text="selectedCityObject ? selectedCityObject.name_{{ app()->getLocale() }} : '{{ __('search_kuwait') }}' "></span>
+                            <span> (</span><span x-text="advertise.total"></span><span> {{ __('ads_title') }})</span>
+                        </small>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <section>
-        <div class="container">
-            <div class="row">
-{{--                @include('site.sections.card')--}}
+        <section>
+            <div class="container">
+                <div class="row">
+                    @include('site.sections.card')
+                </div>
             </div>
-        </div>
-    </section>
-</div>
+        </section>
+    </div>
 
 @endsection
--- resources/views/site/sections/search.blade.php
+++ resources/views/site/sections/search.blade.php
@@ -3,8 +3,8 @@
         <div class="mt-10">
             <div class="row">
                 <div class="col-5 col-lg-6 my_search mr10" >
-                    <select class="input select_input">
-                        <option>{{__('all')}}</option>
+                    <select class="input select_input" x-model="selectedCity">
+                        <option @click="selectedCityObject = null;">{{__('all')}}</option>
                         <template x-for="city in areas" >
                             <optgroup :label="city.name_{{ app()->getLocale() }}">
                                 <template x-for="cityArea in city.areas">
@@ -15,14 +15,14 @@
                     </select>
                 </div>
                 <div class="col-3 col-lg-4 my_search">
-                    <select class="input select_input">
-                        <option>{{__('all')}}</option>
+                    <select class="input select_input" x-model="selectedType">
+                        <option @click="selectedTypeObject = null;">{{__('all')}}</option>
                         <template x-for="typeItem in types">
                             <option :value="typeItem.id" x-text="typeItem.title_{{ app()->getLocale() }}"></option>
                         </template>
                     </select>
                 </div>
-                <div class="col-3 col-lg-2 my_search"><button class="btn btn_lg">{{__('search')}}</button></div>
+                <div class="col-3 col-lg-2 my_search"><button class="btn btn_lg" @click="search();">{{__('search')}}</button></div>
             </div>
         </div>
     </div>
