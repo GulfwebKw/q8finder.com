7f90e7eaa2b23b2be3eac5ff634107e99477cf66
erfan ebrahimi <me@erfanebrahimi.ir>
2023-11-14 11:59:13 +0330

    fix verify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,27 @@
 @extends('layouts.app')
 
+@section('disableHeaderNavbar' , 'yes')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    <section class="mt-30">
+        <div class="container">
+            <div class="row">
+                <!-- Gallery -->
+                <div class="col-lg-3 mob_hide">&nbsp;</div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
+                <div class="col-12 col-lg-6">
+                    <div class="seach_container">
+                        <h3 class="text-center">{{ __('Verify Your Email Address') }}</h3>
+                        @if (session('resent'))
+                            <div class="alert alert-success" role="alert">
+                                {{ __('A fresh verification link has been sent to your email address.') }}
+                            </div>
+                        @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                        {{ __('Before proceeding, please check your email for a verification link.') }}
+                        {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
 @endsection
