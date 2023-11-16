95dfc2185f0206a153452201feeaaceeea5d8904
Erfan Ebrahimi <persionhost@gmail.com>
2023-11-16 23:24:49 +0330

    fix profile

--- resources/views/site/pages/profile.blade.php
+++ resources/views/site/pages/profile.blade.php
@@ -15,62 +15,36 @@
     enctype="multipart/form-data">
     @csrf
     <div class="row">
-
         <div class="col-lg-6 col-md-6 col-xs-12">
-            <div class="mdc-text-field mdc-text-field--outlined w-100">
-                <input type="text" name="name" id="name"
-                    class="mdc-text-field__input @error('name') is-invalid @enderror" placeholder="{{__('your_name')}}"
-                    value="{{ old('name' , auth()->user()->name)}}">
-                <div class="mdc-notched-outline">
-                    <div class="mdc-notched-outline__leading"></div>
-                    <div class="mdc-notched-outline__notch">
-                        <label class="mdc-floating-label">{{__('full_name_title')}}</label>
-                    </div>
-                    <div class="mdc-notched-outline__trailing"></div>
-                </div>
-            </div>
+                <label>{{__('your_name')}}</label>
+                <input type="text" dir="rtl" class="input text-right @error('name') is-invalid @enderror"
+                       value="{{ old('name' , auth()->user()->name)}}" name="name"
+                        placeholder="{{__('your_name')}}">
             @error('name')
-            <span class="invalid-feedback warn-color">
+            <div class="invalid-feedback warn-color">
                 <strong>{{ $message }}</strong>
-            </span>
+            </div>
             @enderror
         </div>
     </div>
     <div class="row mt-3">
         <div class="col-lg-6 col-md-6 col-xs-12">
-            <div class="mdc-text-field mdc-text-field--outlined w-100">
-                <input type="text" disabled class="mdc-text-field__input" value="{{  auth()->user()->mobile}}">
-                <div class="mdc-notched-outline">
-                    <div class="mdc-notched-outline__leading"></div>
-                    <div class="mdc-notched-outline__notch">
-                        <label class="mdc-floating-label">{{__('phone_number_title')}}</label>
-                    </div>
-                    <div class="mdc-notched-outline__trailing"></div>
-                </div>
-            </div>
+            <label>{{__('phone_number_title')}}</label>
+            <input type="text" dir="ltr" disabled class="input text-left"
+                   value="{{  auth()->user()->mobile}}">
         </div>
-
-
     </div>
 
     <div class="row mt-3">
         <div class="col-lg-6 col-md-6 col-xs-12">
-            <div class="mdc-text-field mdc-text-field--outlined w-100">
-                <input type="email" name="email" id="email"
-                    class="mdc-text-field__input @error('email') is-invalid @enderror"
-                    placeholder="{{__('email_address_title')}}" value="{{ old('email' , auth()->user()->email)}}">
-                <div class="mdc-notched-outline">
-                    <div class="mdc-notched-outline__leading"></div>
-                    <div class="mdc-notched-outline__notch">
-                        <label class="mdc-floating-label">{{__('email_address_title')}}</label>
-                    </div>
-                    <div class="mdc-notched-outline__trailing"></div>
-                </div>
-            </div>
+            <label>{{__('email_address_title')}}</label>
+            <input type="email" dir="ltr" class="input text-left @error('email') is-invalid @enderror"
+                   value="{{ old('email' , auth()->user()->email)}}" name="email"
+                    placeholder="{{__('email_address_title')}}">
             @error('email')
-            <span class="invalid-feedback warn-color">
+            <div class="invalid-feedback warn-color">
                 <strong>{{ $message }}</strong>
-            </span>
+            </div>
             @enderror
         </div>
     </div>
@@ -81,45 +55,27 @@
 
     <div class="row mt-3">
         <div class="col-lg-6 col-md-6 col-xs-12">
-            <div class="mdc-text-field mdc-text-field--outlined w-100">
-                <input type="text" name="company_name" id="company_name"
-                    class="mdc-text-field__input @error('company_name') is-invalid @enderror"
-                    placeholder="{{__('company_name_title')}}"
-                    value="{{ old('company_name' , auth()->user()->company_name)}}">
-                <div class="mdc-notched-outline">
-                    <div class="mdc-notched-outline__leading"></div>
-                    <div class="mdc-notched-outline__notch">
-                        <label class="mdc-floating-label">{{__('company_name_title')}}</label>
-                    </div>
-                    <div class="mdc-notched-outline__trailing"></div>
-                </div>
-            </div>
+            <label>{{__('company_name_title')}}</label>
+            <input type="text" dir="rtl" class="input text-right @error('company_name') is-invalid @enderror"
+                   value="{{ old('company_name' , auth()->user()->company_name)}}" name="company_name"
+                    placeholder="{{__('company_name_title')}}">
             @error('company_name')
-            <span class="invalid-feedback warn-color">
+            <div class="invalid-feedback warn-color">
                 <strong>{{ $message }}</strong>
-            </span>
+            </div>
             @enderror
         </div>
     </div>
     <div class="row mt-3">
         <div class="col-lg-6 col-md-6 col-xs-12">
-            <div class="mdc-text-field mdc-text-field--outlined w-100">
-                <input type="text" name="company_phone" id="company_phone"
-                    class="mdc-text-field__input @error('company_phone') is-invalid @enderror"
-                    placeholder="{{__('company_phone_title')}}"
-                    value="{{ old('company_phone' , auth()->user()->company_phone)}}">
-                <div class="mdc-notched-outline">
-                    <div class="mdc-notched-outline__leading"></div>
-                    <div class="mdc-notched-outline__notch">
-                        <label class="mdc-floating-label">{{__('company_phone_title')}}</label>
-                    </div>
-                    <div class="mdc-notched-outline__trailing"></div>
-                </div>
-            </div>
+            <label>{{__('company_phone_title')}}</label>
+            <input type="text" dir="ltr" class="input text-left @error('company_phone') is-invalid @enderror"
+                   value="{{ old('company_phone' , auth()->user()->company_phone)}}" name="company_phone"
+                   placeholder="{{__('company_phone_title')}}">
             @error('company_phone')
-            <span class="invalid-feedback warn-color">
+            <div class="invalid-feedback warn-color">
                 <strong>{{ $message }}</strong>
-            </span>
+            </div>
             @enderror
         </div>
     </div>
@@ -127,42 +83,25 @@
 
     <div class="row mt-3">
         <div class="col-lg-6 col-md-6 col-xs-12">
-            <div class="mdc-text-field mdc-text-field--outlined w-100">
-                <input type="email" name="social_email" id="social_email" placeholder="{{__('company_email')}}"
-                    class="mdc-text-field__input @error('social_email') is-invalid @enderror"
-                    value="{{ old('social_email' , optional(auth()->user()->socials()->where('type', 'email')->first())->address)}}">
-                <div class="mdc-notched-outline">
-                    <div class="mdc-notched-outline__leading"></div>
-                    <div class="mdc-notched-outline__notch">
-                        <label class="mdc-floating-label">{{__('company_email')}}</label>
-                    </div>
-                    <div class="mdc-notched-outline__trailing"></div>
-                </div>
-            </div>
+            <label>{{__('company_email')}}</label>
+            <input type="email" dir="ltr" class="input text-left @error('social_email') is-invalid @enderror"
+                   value="{{ old('social_email' , auth()->user()->social_email)}}" name="social_email"
+                   placeholder="{{__('company_email')}}">
             @error('social_email')
-            <span class="invalid-feedback warn-color">
+            <div class="invalid-feedback warn-color">
                 <strong>{{ $message }}</strong>
-            </span>
+            </div>
             @enderror
         </div>
         <div class="col-lg-6 col-md-6 col-xs-12">
-            <div class="mdc-text-field mdc-text-field--outlined w-100">
-                <input type="text" name="instagram" id="instagram"
-                    class="mdc-text-field__input @error('instagram') is-invalid @enderror"
-                    placeholder="{{__('instagram_address_title')}}"
-                    value="{{ old('instagram' , optional(auth()->user()->socials()->where('type', 'instagram')->first())->address)}}">
-                <div class="mdc-notched-outline">
-                    <div class="mdc-notched-outline__leading"></div>
-                    <div class="mdc-notched-outline__notch">
-                        <label class="mdc-floating-label">{{__('instagram_address_title')}}</label>
-                    </div>
-                    <div class="mdc-notched-outline__trailing"></div>
-                </div>
-            </div>
+            <label>{{__('instagram_address_title')}}</label>
+            <input type="text" dir="ltr" class="input text-left @error('instagram') is-invalid @enderror"
+                   value="{{ old('instagram' , auth()->user()->instagram)}}" name="instagram"
+                   placeholder="{{__('instagram_address_title')}}">
             @error('instagram')
-            <span class="invalid-feedback warn-color">
+            <div class="invalid-feedback warn-color">
                 <strong>{{ $message }}</strong>
-            </span>
+            </div>
             @enderror
         </div>
     </div>
@@ -170,23 +109,14 @@
 
     <div class="row mt-3">
         <div class="col-lg-6 col-md-6 col-xs-12">
-            <div class="mdc-text-field mdc-text-field--outlined w-100">
-                <input type="text" name="twitter" id="twitter"
-                    class="mdc-text-field__input @error('twitter') is-invalid @enderror"
-                    placeholder="{{__('twitter_address_title')}}"
-                    value="{{ old('twitter' , optional(auth()->user()->socials()->where('type', 'twitter')->first())->address)}}">
-                <div class="mdc-notched-outline">
-                    <div class="mdc-notched-outline__leading"></div>
-                    <div class="mdc-notched-outline__notch">
-                        <label class="mdc-floating-label">{{__('twitter_address_title')}}</label>
-                    </div>
-                    <div class="mdc-notched-outline__trailing"></div>
-                </div>
-            </div>
+            <label>{{__('twitter_address_title')}}</label>
+            <input type="text" dir="ltr" class="input text-left @error('twitter') is-invalid @enderror"
+                   value="{{ old('twitter' , auth()->user()->twitter)}}" name="twitter"
+                   placeholder="{{__('twitter_address_title')}}">
             @error('twitter')
-            <span class="invalid-feedback warn-color">
+            <div class="invalid-feedback warn-color">
                 <strong>{{ $message }}</strong>
-            </span>
+            </div>
             @enderror
         </div>
     </div>
@@ -195,37 +125,35 @@
     <hr class="mt-3">
     @endif
     <div class="row mt-3">
+{{--        <div class="col-lg-6 col-md-6 col-xs-12">--}}
+{{--             <div class="mdc-text-field--outlined w-100">--}}
+{{--                 <label class="text-muted w-100">{{__('get_verified_profile_title')}}</label>--}}
+{{--                 <input type="file" name="licence" id="licence"--}}
+{{--                        class="mdc-text-field__input @error('licence') is-invalid @enderror"--}}
+{{--                        placeholder="{{__('get_verified_profile_title')}}">--}}
+{{--             </div>--}}
+{{--            @error('licence')--}}
+{{--            <span class="invalid-feedback warn-color">--}}
+{{--                <strong>{{ $message }}</strong>--}}
+{{--            </span>--}}
+{{--            @enderror--}}
+{{--        </div>--}}
         <div class="col-lg-6 col-md-6 col-xs-12">
-            {{-- <div class="mdc-text-field--outlined w-100">--}}
-                {{-- <label class="text-muted w-100">{{__('get_verified_profile_title')}}</label>--}}
-                {{-- <input type="file" name="licence" id="licence" --}} {{--
-                    class="mdc-text-field__input @error('licence') is-invalid @enderror" --}} {{--
-                    placeholder="{{__('get_verified_profile_title')}}">--}}
-                {{-- </div>--}}
-            @error('licence')
-            <span class="invalid-feedback warn-color">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-        </div>
-        <div class="col-lg-6 col-md-6 col-xs-12">
-            <div class="mdc-text-field--outlined w-100">
-                <label class="text-muted w-100">{{__('profile_image_title')}}</label>
-                <input type="file" name="avatar" id="avatar"
-                    class="mdc-text-field__input @error('avatar') is-invalid @enderror"
-                    placeholder="{{__('profile_image_title')}}">
-            </div>
+            <label>{{__('profile_image_title')}}</label>
+            <input type="file" class="input @error('avatar') is-invalid @enderror"
+                   value="{{ old('avatar' , auth()->user()->avatar)}}" name="avatar"
+                   placeholder="{{__('profile_image_title')}}">
             @error('avatar')
-            <span class="invalid-feedback warn-color">
+            <div class="invalid-feedback warn-color">
                 <strong>{{ $message }}</strong>
-            </span>
+            </div>
             @enderror
         </div>
 
     </div>
 
-    <button type="submit" class="mdc-button mdc-button--raised mdc-ripple-upgraded w-100"
-        style="margin-top:30px;">{{__('save_title')}} &amp; {{__('upload_title')}}</button>
+    <button type="submit" class="btn btn_lg"
+        style="margin-top:30px !important;">{{__('save_title')}} &amp; {{__('upload_title')}}</button>
 </form>
 
 {{-- <button type="submit" class="mdc-button mdc-button--raised mdc-ripple-upgraded bg-danger"
