23a068fb74c70447ba941cd90ebab499066062ed
Erfan Ebrahimi <persionhost@gmail.com>
2023-11-18 20:48:56 +0330

    fix buy pacakge

--- resources/views/site/pages/buyPackage.blade.php
+++ resources/views/site/pages/buyPackage.blade.php
@@ -67,118 +67,117 @@
                 <div class="mdc-tab-bar-wrapper centered pricing-tabs">
                     @if ( ! env('NORMAL_ADS_FREE' , false) )
                         <ul  class="d-flex justify-content-center nav" role="tablist" style="column-gap: 20px;">
-                            <li role="presentation" class="active p-3 rounded"><a href="#payasyougo" aria-controls="payasyougo" role="tab" data-toggle="tab">{{__('payasyougo')}}</a></li>
-                            <li role="presentation" class=" p-3 rounded"><a href="#longtermsubscribe" aria-controls="longtermsubscribe" role="tab" data-toggle="tab">{{__('longtermsubscribe')}}</a></li>
+                            <li role="presentation" style="cursor: pointer;" class="menuTab active p-3 rounded" onclick="$('.tab-pane').hide();$('#payasyougo').show();$('.menuTab').removeClass('active');$(this).addClass('active');">{{__('payasyougo')}}</li>
+                            <li role="presentation" style="cursor: pointer;" class="menuTab p-3 rounded" onclick="$('.tab-pane').hide();$('#longtermsubscribe').show();$('.menuTab').removeClass('active');$(this).addClass('active');">{{__('longtermsubscribe')}}</li>
                         </ul>
                     @endif
-                    <div class="tab-content tab-content--active">
-                        <div class="row">
-                            @foreach($statics as $static)
-                            <div class="col-xs-12 col-sm-12 col-md-6 p-2">
-                                <form method="post" action="{{ route('Main.buyPackageOrCredit',app()->getLocale()) }}"
-                                    class="d-flex">
-                                    <div class="w-100">
-                                        @csrf
-                                        <div class="mdc-card pricing-card text-center border-accent p-0 h-100" style="border-color: #fff;">
-                                            <div class="bg-accent pricing-header px-2 py-4 rounded" style="background-color: #06090c70;">
-                                                <h1 class="fs-sm-20">@if( $static->old_price > $static->price ) <small>
-                                                        <span class="del opacity-70">{{ $static->old_price }} </span>
-                                                    </small> @endif {{$static->price }}<small> {{__('kd_title')}}
-                                                        <br>
-                                                        {{-- {{ app()->getLocale()=="en" ? $static->title_en :
-                                                        $static->title_ar }} --}}
-                                                        {{-- /{{ $static->count_day }} {{__('days')}} --}}</small></h1>
-                                                <p class="desc mb-2">
-                                                    @if(app()->getLocale()=="en"){{$static->title_en}}@else{{$static->title_ar}}@endif
-                                                </p>
-                                            </div>
-                                            <div class="px-2 ad-plan-bottom">
-                                                <p class=" add-plan-description">
-                                                    @if(app()->getLocale()=="en"){{$static->description_en}}@else{{$static->description_ar}}@endif
-                                                </p>
-
-                                                <div>
-                                                    <input type="hidden" class="form-control" name="payment_type"
-                                                        value="CBKPay">
-
-                                                    <label>{{__('noofads')}}</label>
-                                                    <input type="number" dir="ltr" class="input text-left "
-                                                           value="1"  name="count" min="1" id="{{ "
-                                                            static-num-" . $static->id }}" required
-                                                           placeholder="{{__('noofads')}}">
+                    <div class="tab-content">
+                        <div class="tab-pane" style="display: block;" id="payasyougo">
+                            <div class="row">
+                                @foreach($statics as $static)
+                                <div class="col-xs-12 col-sm-12 col-md-6 p-2">
+                                    <form method="post" action="{{ route('Main.buyPackageOrCredit',app()->getLocale()) }}"
+                                        class="d-flex">
+                                        <div class="w-100">
+                                            @csrf
+                                            <div class="mdc-card pricing-card text-center border-accent p-0 h-100" style="border-color: #fff;">
+                                                <div class="bg-accent pricing-header px-2 py-4 rounded" style="background-color: #06090c70;">
+                                                    <h1 class="fs-sm-20">@if( $static->old_price > $static->price ) <small>
+                                                            <span class="del opacity-70">{{ $static->old_price }} </span>
+                                                        </small> @endif {{$static->price }}<small> {{__('kd_title')}}
+                                                            <br>
+                                                            {{-- {{ app()->getLocale()=="en" ? $static->title_en :
+                                                            $static->title_ar }} --}}
+                                                            {{-- /{{ $static->count_day }} {{__('days')}} --}}</small></h1>
+                                                    <p class="desc mb-2">
+                                                        @if(app()->getLocale()=="en"){{$static->title_en}}@else{{$static->title_ar}}@endif
+                                                    </p>
                                                 </div>
-                                                <input type="hidden" name="type" value="static">
-                                                <input type="hidden" name="package_id" value="{{ $static->id }}">
+                                                <div class="px-2 ad-plan-bottom">
+                                                    <p class=" add-plan-description">
+                                                        @if(app()->getLocale()=="en"){{$static->description_en}}@else{{$static->description_ar}}@endif
+                                                    </p>
 
+                                                    <div>
+                                                        <input type="hidden" class="form-control" name="payment_type"
+                                                            value="CBKPay">
+
+                                                        <label>{{__('noofads')}}</label>
+                                                        <input type="number" dir="ltr" class="input text-left "
+                                                               value="1"  name="count" min="1" id="{{ "
+                                                                static-num-" . $static->id }}" required
+                                                               placeholder="{{__('noofads')}}">
+                                                    </div>
+                                                    <input type="hidden" name="type" value="static">
+                                                    <input type="hidden" name="package_id" value="{{ $static->id }}">
+
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="w-100">
-                                        <div class="c w-100 d-flex justify-center" style="height: calc(100% - 50px);align-items: center;">
-                                            <div class="text-center f-d-column w-100">
-                                                <div class="d-block pb-2">
-                                                    <div class="c" style="cursor: pointer;">
-                                                        <div onclick='{{ $credit["count_premium_advertising"] }} > 0 ? (location.href = "/"+"{{ app()->getLocale() }}"+"/advertising/create?type=premium&show_ad_type_option=true") : alert("{{ trans("have_no_package") }}")'
-                                                            class="pe-auto bg-orange rounded-full ar-eq w-30px c d-inline-block mx-auto">
-                                                            <i class="fa fa-plus-circle fa-2x" style="color: #ff9800"></i>
+                                        <div class="w-100">
+                                            <div class="c w-100 d-flex justify-center" style="height: calc(100% - 50px);align-items: center;">
+                                                <div class="text-center f-d-column w-100">
+                                                    <div class="d-block pb-2">
+                                                        <div class="c" style="cursor: pointer;">
+                                                            <div onclick='{{ $credit["count_premium_advertising"] }} > 0 ? (location.href = "/"+"{{ app()->getLocale() }}"+"/advertising/create?type=premium&show_ad_type_option=true") : alert("{{ trans("have_no_package") }}")'
+                                                                class="pe-auto bg-orange rounded-full ar-eq w-30px c d-inline-block mx-auto">
+                                                                <i class="fa fa-plus-circle fa-2x" style="color: #ff9800;margin-left: 0;"></i>
+                                                            </div>
                                                         </div>
                                                     </div>
+                                                    <p>{{ __('add_credit') }}</p>
                                                 </div>
-                                                <p>{{ __('add_credit') }}</p>
+                                            </div>
+                                            <div class="w-100 px-2" style="height: 40px">
+                                                <button type="submit" class="btn btn_lg d-block"
+                                                    style="margin-left: auto;max-width: 100px;">{{__('buy')}}
+                                                </button>
                                             </div>
                                         </div>
-                                        <div class="w-100 px-2" style="height: 40px">
-                                            <button type="submit" class="btn btn_lg d-block"
-                                                style="margin-left: auto;max-width: 100px;">{{__('buy')}}
-                                            </button>
+                                    </form>
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="tab-pane" id="longtermsubscribe">
+                            <div class="row">
+                                @foreach($normals as $normal)
+                                <div class="col-xs-12 col-sm-6 col-md-3 p-2">
+                                    <div class="mdc-card pricing-card text-center border-accent p-0 h-100">
+                                        <div class="bg-accent pricing-header px-2 py-4 rounded" style="background-color: #06090c70;">
+                                            <h1 class="fs-sm-20"> @if( $normal->old_price > $normal->price ) <small> <span
+                                                        class="del opacity-70">{{ $normal->old_price }} </span></small>
+                                                @endif {{ $normal->price }}<small> {{__('kd_title')}}
+                                                    <br>
+                                                    {{-- {{ $normal->count_day }} {{__('days')}} --}}
+                                                </small></h1>
+                                            <p class="desc mb-2">
+                                                @if(app()->getLocale()=="en"){{$normal->title_en}}@else{{$normal->title_ar}}@endif
+                                            </p>
+                                        </div>
+                                        <div class="p-3">
+                                            <p class="py-2">
+                                                <!--<span class="mx-2 fw-500">10</span>-->
+                                                @if(app()->getLocale()=="en"){{$normal->description_en}}@else{{$normal->description_ar}}@endif
+                                            </p>
+
+                                            <form method="post"
+                                                action="{{ route('Main.buyPackageOrCredit',app()->getLocale()) }}">
+                                                @csrf
+                                                <input type="hidden" class="form-control" name="payment_type"
+                                                    value="CBKPay">
+                                                <input type="hidden" name="type" value="normal">
+                                                <input type="hidden" name="package_id" value="{{ $normal->id }}">
+                                                <button type="submit"  class="btn btn_lg d-block"
+                                                        style="max-width: 100px;">{{__('buy')}}</button>
+                                            </form>
                                         </div>
                                     </div>
-                                </form>
-                            </div>
-                            @endforeach
-                        </div>
-                    </div>
-                    <div class="tab-content">
-                        <div class="row">
-                            @foreach($normals as $normal)
-                            <div class="col-xs-12 col-sm-6 col-md-3 p-2">
-                                <div class="mdc-card pricing-card text-center border-accent p-0 h-100">
-                                    <div class="bg-accent pricing-header p-2">
-                                        <h1 class="fs-sm-20"> @if( $normal->old_price > $normal->price ) <small> <span
-                                                    class="del opacity-70">{{ $normal->old_price }} </span></small>
-                                            @endif {{ $normal->price }}<small> {{__('kd_title')}}
-                                                <br>
-                                                {{-- {{ $normal->count_day }} {{__('days')}} --}}
-                                            </small></h1>
-                                        <p class="desc mb-2">
-                                            @if(app()->getLocale()=="en"){{$normal->title_en}}@else{{$normal->title_ar}}@endif
-                                        </p>
-                                    </div>
-                                    <div class="p-3">
-                                        <p class="py-2">
-                                            <!--<span class="mx-2 fw-500">10</span>-->
-                                            @if(app()->getLocale()=="en"){{$normal->description_en}}@else{{$normal->description_ar}}@endif
-                                        </p>
-
-                                        <form method="post"
-                                            action="{{ route('Main.buyPackageOrCredit',app()->getLocale()) }}">
-                                            @csrf
-                                            <input type="hidden" class="form-control" name="payment_type"
-                                                value="CBKPay">
-                                            <input type="hidden" name="type" value="normal">
-                                            <input type="hidden" name="package_id" value="{{ $normal->id }}">
-                                            <button type="submit" class="mdc-button mdc-button--raised">
-                                                <span class="mdc-button__ripple"></span>
-                                                <span class="mdc-button__label">{{__('buy')}}</span>
-                                            </button>
-                                        </form>
-                                    </div>
                                 </div>
+                                @endforeach
                             </div>
-                            @endforeach
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
