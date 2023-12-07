0d2dc55589651bf8b08b0c1232041a6041c2f79b
Erfan Ebrahimi <persionhost@gmail.com>
2023-12-07 22:41:45 +0330

    fix search in main page
    https://trello.com/c/A5uj6yYA/278-area-drop-down-menu

--- resources/views/site/sections/search.blade.php
+++ resources/views/site/sections/search.blade.php
@@ -1,10 +1,60 @@
 <section>
+    <style>
+        @media all and (max-width: 480px) and (min-width: 320px) {
+            .select2-container--open {
+                position: fixed;
+                width: calc(100% - 2px) !important;
+                top: 0;
+                left: 0;
+                right: 0;
+            }
+            .select2-dropdown {
+                position: fixed;
+                width:  calc(100% - 4px);
+                height: calc(100% - 47px);
+                /*height: calc(80% - 47px);*/
+                top: 47px;
+            }
+            .select2-container--default .select2-results>.select2-results__options {
+                max-height: 100%;
+                height: 100% ;
+            }
+            .select2-container--default .select2-results {
+                /*max-height: 100%;*/
+                /*height: 100% ;*/
+                height: calc(100% - 47px);
+                max-height: calc(100% - 47px);
+            }
+        }
+        .select2-container--default .select2-results__option .select2-results__option {
+            display: grid;
+        }
+        .select2-results__option--group li {
+            margin-right: 20px ;
+        }
+    </style>
+    <script>
+        function formatState (state) {
+            if (!state.id) {
+                return state.text;
+            }
+            var $state = $(
+                '<div class="wwwwwww">' +
+                '<span style="float: right">' + state.text + '</span>' +
+                '<span style="float: left">(' + state.element.dataset.numad + ')</span>' +
+                '</div>'
+            );
+            return $state;
+        };
+    </script>
     <div class="container">
         <div class="mt-10">
-            <div class="row">
-                <div class="col-5 col-lg-6 my_search mr10" >
+            <div class="row search-form-for-mobile">
+                <div class="col-5 col-lg-6 my_search" >
                     <select x-init="
-      $($refs.selectField).select2().on('change', function (e) {
+      $($refs.selectField).select2({
+  templateResult: formatState
+}).on('change', function (e) {
         selectedCity = Array.from(e.target.options).filter(option => option.selected).map(option => option.value);
       });
 	" x-ref="selectField"  class="input select_input multiple-select2" multiple  >
@@ -12,7 +62,10 @@
                         <template x-for="city in areas" >
                             <optgroup :label="city.name_{{ app()->getLocale() }}">
                                 <template x-for="cityArea in city.areas">
-                                    <option :value="cityArea.id" x-text="cityArea.name_{{ app()->getLocale() }}"></option>
+                                    <option
+                                        :value="cityArea.id"
+                                        x-bind:data-numad="cityArea.advertising_count"
+                                            x-text="cityArea.name_{{ app()->getLocale() }}"></option>
                                 </template>
                             </optgroup>
                         </template>
@@ -26,7 +79,7 @@
                         </template>
                     </select>
                 </div>
-                <div class="col-3 col-lg-2 my_search"><button class="btn btn_lg" @click="advertise = [];page=1;totalPage=1;totalAdvertise=0;search();">{{__('search')}}</button></div>
+                <div class="col-3 col-lg-2 my_search"><button class="btn btn_lg" @click="advertise = [];page=1;totalPage=1;totalAdvertise=0;search();" style="padding-right: 0 !important;padding-left: 0 !important;text-align: center;">{{__('search')}}</button></div>
             </div>
 
             @if (request()->is(app()->getLocale().'/required'))
